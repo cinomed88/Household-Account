8100fe75dbec9243faa7817036e5bc5ec943de18
cinomed88 <gongthebest@gmail.com>
2019-07-14 22:32:45 -0700

    date recognization

---
+++ static/js/date.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+var d = new Date();
+
+var week = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
+
+var month = new Array('January', 'February', 'March' , 'April', 'May', 
+    'June', 'July', 'August', 'September', 'October', 'November', 'December');
+
+$("#heading2").text( month[d.getMonth()] + ", " + d.getDate()
+    + " (" + week[d.getDay()] + ")" );
+
+{/* document.write('현재 년: ' + d.getFullYear() + '<br />');
+document.write('현재 월: ' + (d.getMonth() + 1) + '<br />');
+document.write('현재 일: ' + d.getDate() + '<br />');
+
+document.write('<br />'); // 줄바꿈
+
+document.write('현재 시: ' + d.getHours() + '<br />');
+document.write('현재 분: ' + d.getMinutes() + '<br />');
+document.write('현재 초: ' + d.getSeconds() + '<br />');
+
+document.write('<br />');
+
+document.write('오늘 요일: ' + d.getDay() + '<br />'); // 일요일 = 0 */}
+});
