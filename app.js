2fd7f8ac00971ab0afa6dc2edd6a97d9205afede
cinomed88 <gongthebest@gmail.com>
2019-07-18 20:17:45 -0700

    input func

--- app.js
+++ app.js
@@ -1,7 +1,46 @@
 var fs = require('fs');
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'pp1'
+});
 
+connection.connect(function(err){
+    if (err) throw err;
+
+    console.log('Connection succeed!');
+});
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.post('/post-submit', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    console.log("Stuff sent to server", req.body);
+    res.send(["You sent me:", req.body]);
+
+    // console.log(req.body);
+    // var sql = "INSERT INTO record values('')";
+
+
+    // connection.query(sql, function (err) {
+    //     if (err) throw err;
+
+    //     console.log('error now occur');
+    // })
+});
+
+
+connection.end();
 
 app.use('/css', express.static('static/css'));
 app.use('/img', express.static('static/img'));
@@ -14,7 +53,8 @@ app.get('/', (req, res) => {
 });
 
 
-var port = 8000;
+
+var port = 8001;
 app.listen(port, () => {
     console.log("This app is listening on port " + port + "!");
 });
---
+++ static/js/submit.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    var d = new Date();
+    var ctime = d.getFullYear() + "-" + (d.getMonth() + 1)
+     + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes()
+     + ":" + d.getSeconds();
+
+    $('#submit').click(function(e) {
+        let formData = { time: ctime,
+                         description: $("#description_input").val(),
+                         expense: $("#expense_input").val()
+                       };
+        console.log("Form data to send:", formData);
+        $.ajax({
+            url: "/post-submit",
+            dataType: "json",
+            type: "POST",
+            data: formData,
+            success: function(data) {
+                console.log("SUCCESS JSON:", data);
+                $("#p2").html(data[0] + " " + data[1]['description']
+                              + " " + data[1]['expense']
+                             );
+
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                $("#p2").text(jqXHR.statusText);
+                console.log("ERROR:", jqXHR, textStatus, errorThrown);
+            }
+        });
+    });
+});
